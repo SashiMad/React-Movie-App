1216122838f242ff3686d249a54e9de20acdb960
Sashi <103021999+Sashicake@users.noreply.github.com>
2022-06-28 14:05:11 +0200

    added search component, integrated searched value into the api and the functions

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MovieList from "./MovieList";
+import MovieListHeading from "./MovieListHeading";
+import SearchBox from "./SearchBox";
 import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState(" ");
 
-  const getMovieRequest = async () => {
-    const url = "http://www.omdbapi.com/?s=star wars&apikey=2cef477e";
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=2cef477e`;
 
     const response = await fetch(url);
     const responseJson = await response.json();
 
-    console.log(responseJson);
-    setMovies(responseJson.Search);
+    if (responseJson.Search) {
+      setMovies(responseJson.Search);
+    }
   };
 
   useEffect(() => {
-    getMovieRequest();
-  }, []);
+    getMovieRequest(searchValue);
+  }, [searchValue]);
 
   return (
     <div className="container-fluid movie-app ">
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Movies" />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+      </div>
+
       <div className="row ">
         <MovieList movies={movies} />
       </div>
